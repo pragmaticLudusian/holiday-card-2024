6b7239ccbc63c34d296acf447e8e362721142c1a
pragmaticLudusian <arsen.r.m@gmail.com>
2024-12-19 00:24:57 +0200

    feature: change card size dynamically

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,5 +1,29 @@
-const cardElement = document.querySelector(".card");
+const songArray = [
+  "uceNmJSyXGw",
+  "pW-RcN7mxZc",
+  "Y_GlTjwPjFA",
+  "88InoQyyP3w",
+  "MiH7NaaqtTs",
+  "e3KrI4lBeps",
+  "lGlhq-0uyfA",
+  "kJuWPRq-mwM",
+  "zd2i3UAXm44",
+  "jnxwkFMJjts",
+  "bUIkTPzkLHE",
+  "sRy0RJ96gxM",
+];
 
-cardElement.addEventListener("click", (event) => {
-  console.log(event.target);
+const cssRoot = document.querySelector(":root");
+const cssRootStyle = getComputedStyle(cssRoot);
+cardHeightRender();
+
+const cardElement = document.querySelector(".card__front");
+cardElement.addEventListener("click", () => {
+  const randomNum = Math.floor(Math.random() * 12);
+  console.log(`https://youtu.be/${songArray[randomNum]}`);
 });
+
+window.addEventListener("resize", cardHeightRender);
+function cardHeightRender() {
+  cssRoot.style.setProperty("--cardheight", `${window.innerHeight * 0.8}px`);
+}
